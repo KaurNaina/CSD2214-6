75dd4e493c3b9d9d4a8fbe83486d1027c9748a1a
KaurNaina <46692995+KaurNaina@users.noreply.github.com>
2019-01-14 17:03:29 -0500

    WEEK 2 CLASS ACTIVITY

---
+++ Script1.js
@@ -0,0 +1,15 @@
+//Name:Satinder Kaur
+//Date:January 14,2019
+//Student:C0739221
+//Course:CSD 2214 Section 6
+
+
+
+let countDown;
+let timeInterval;
+const endDate = documents.querySelector('input[name="endDate"]');
+const clock = document.querySelector('clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
